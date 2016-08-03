87cf8c7e10a36c548dccd6b0485687a3ed594b49
Nirmal Thacker <nirmalthacker@gmail.com>
2016-08-03 09:19:01 -0700

    a better partition for quicksort

--- quicksort.py
+++ quicksort.py
@@ -3,39 +3,32 @@ Quicksort!
 '''
 
 def swap(a, i, j):
-	t = a[i]
-	a[i] = a[j]
-	a[j] = t
+	if i != j:
+		t = a[i]
+		a[i] = a[j]
+		a[j] = t
 	return
 
-
-def partition(a, low, high):
+def partition_2(a, low, high):
+	#from Programming Pearls
 	pivot = a[low]
-	start = low+1
-	end = high
-	done = False
-	while not done:
-		while start <= end and a[start] <= pivot:
-			start = start + 1
-		while a[start] >= pivot and end >= start:
-			end = end - 1
-		if start > end:
-			done = True
-		else:
-			swap(a, start, end)
-	swap(a, low, end)
-	return end
-
-
+	m = low
+	for i in range(low+1, high+1):
+		if a[i] < pivot:
+			m = m + 1
+			swap(a, m, i)
+	swap(a, low, m)
+	return m
 
 def quicksort(a, low, high):
 	if low < high:
-		p= partition(a, low, high)
+		p= partition_2(a, low, high)
 		quicksort(a, low, p-1)
 		quicksort(a, p+1, high)
 	return a
 
-#a = [89, 24, 34, 12, 2, 43, 523, 52, 642, 21, 35, 6, 124, 99]
-a = [3, 1, 0, 4, 7]
+a = [89, 24, 34, 12, 2, 43, 523, 52, 642, 21, 35, 6, 124, 99]
+#a = [4, 2, 5, 6]
 quicksort(a, 0, len(a)-1)
 print(a)
+
