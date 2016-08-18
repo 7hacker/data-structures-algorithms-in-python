28f7176da2eb2c60fa9416ee4e075217423664db
Nirmal Thacker <nirmalthacker@gmail.com>
2016-08-18 15:13:58 -0700

    Longest repeated char substring

--- buildingMinCost.py
+++ buildingMinCost.py
@@ -4,38 +4,38 @@ There are N buildings with variable number of floors, and we want to build addit
 import sys
 
 def buildingMinCost(buildings, m):
-	buildings.sort()
-	window = []
-	minWindowCost = sys.maxint
-	currentWindowCost = 0
-	
-	#set up window
-	for i in xrange(m):
-		window.append(buildings[i])
+    buildings.sort()
+    window = []
+    minWindowCost = sys.maxint
+    currentWindowCost = 0
+    
+    #set up window
+    for i in xrange(m):
+        window.append(buildings[i])
 
-	#compute first window cost
-	for i in xrange(m):
-		currentWindowCost = currentWindowCost+ (window[-1] - window[i])
+    #compute first window cost
+    for i in xrange(m):
+        currentWindowCost = currentWindowCost + (window[-1] - window[i])
 
-	#set it to the minWindowCost
-	if currentWindowCost < minWindowCost:
-		minWindowCost = currentWindowCost
+    #set it to the minWindowCost
+    if currentWindowCost < minWindowCost:
+        minWindowCost = currentWindowCost
 
-	for i in xrange(m,len(buildings)):
-		#drop off the first building
-		currentWindowCost = currentWindowCost - (window[-1] - window[0])
-		window.pop(0)
+    for i in xrange(m,len(buildings)):
+        #drop off the first building
+        currentWindowCost = currentWindowCost - (window[-1] - window[0])
+        window.pop(0)
 
-		#compute the next delta and add the new building
-		nextDelta = buildings[i] - window[-1]
-		window.append(buildings[i])
+        #compute the next delta and add the new building
+        nextDelta = buildings[i] - window[-1]
+        window.append(buildings[i])
 
-		#compute the new window size
-		currentWindowCost = currentWindowCost + (nextDelta * (m-1))
-		if currentWindowCost < minWindowCost:
-			minWindowCost = currentWindowCost
+        #compute the new window size
+        currentWindowCost = currentWindowCost + (nextDelta * (m-1))
+        if currentWindowCost < minWindowCost:
+            minWindowCost = currentWindowCost
 
-	return minWindowCost
+    return minWindowCost
 
 
 print(buildingMinCost([10,3,1,5,11,10],3))
--- rainfallChallenge.py
+++ rainfallChallenge.py
@@ -4,91 +4,89 @@ dat palantir Rain Fall Challenge
 import sys
 
 class RainFallChallenge:
-	
-	def __init__(self, t):
-		self.terrain = t
-		self.size = len(t) #assume size x size terrain is passed
-		self.basins = [[None]*self.size for i in xrange(self.size)]
-		self.currentBasin = 64 #65 becomes 'A'
+    
+    def __init__(self, t):
+        self.terrain = t
+        self.size = len(t) #assume size x size terrain is passed
+        self.basins = [[None]*self.size for i in xrange(self.size)]
+        self.currentBasin = 64 #65 becomes 'A'
 
-		#for x,y the neighbors are : x,y-1 x+1,y x,y+1 x-1,y
-		self.neighborx = [0, 1, 0, -1] #clockwise from North 
-		self.neighbory = [-1, 0, 1, 0]
+        #for x,y the neighbors are : x,y-1 x+1,y x,y+1 x-1,y
+        self.neighborx = [0, 1, 0, -1] #clockwise from North 
+        self.neighbory = [-1, 0, 1, 0]
 
-		self._findSinks()
-		self.showBasin()
-		self._populateBasin(0,0)
-
-
-	def isSafe(self, x, y):
-		if x < self.size and y < self.size and x >=0 and y >=0: 
-			return True
-		else:
-			return False
-
-	def _findSinks(self):
-		#find all the sinks. a sink is defined as being the lowest among its 4 neighbors
-		for i in xrange(self.size):
-			for j in xrange(self.size):
-				vals = [self.terrain[i][j]]
-				for k in xrange(4):
-					nx = i + self.neighborx[k]
-					ny = j + self.neighbory[k]
-					if self.isSafe(nx,ny):
-						vals.append(self.terrain[nx][ny])
-				if min(vals) == self.terrain[i][j]:
-					#a new Basin can be formed, since a sink exists at this point
-					self.currentBasin = self.currentBasin+1
-					self.basins[i][j] = chr(self.currentBasin)
-
-	def _mySmallestNeighbor(self, x, y):
-		minVals = []
-		d = {}
-		for k in xrange(4):
-			nx = x + self.neighborx[k]
-			ny = y + self.neighbory[k]
-			if self.isSafe(nx,ny):
-				minVals.append(self.terrain[nx][ny])
-				d[self.terrain[nx][ny]] = (nx, ny)
-		mn = min(minVals)
-		return d[mn]
+        self._findSinks()
+        self.showBasin()
+        self._populateBasin(0,0)
 
 
+    def isSafe(self, x, y):
+        if x < self.size and y < self.size and x >=0 and y >=0: 
+            return True
+        else:
+            return False
 
-	def _populateBasin(self, row, col):
-		if row >= self.size or col >= self.size:
-			#out of bounds
-			return
+    def _findSinks(self):
+        #find all the sinks. a sink is defined as being the lowest among its 4 neighbors
+        for i in xrange(self.size):
+            for j in xrange(self.size):
+                vals = [self.terrain[i][j]]
+                for k in xrange(4):
+                    nx = i + self.neighborx[k]
+                    ny = j + self.neighbory[k]
+                    if self.isSafe(nx,ny):
+                        vals.append(self.terrain[nx][ny])
+                if min(vals) == self.terrain[i][j]:
+                    #a new Basin can be formed, since a sink exists at this point
+                    self.currentBasin = self.currentBasin+1
+                    self.basins[i][j] = chr(self.currentBasin)
 
-		elif self.basins[row][col] != None:
-			#i am already assigned a basin just move ahead
-			if col+1 < self.size:
-				self._populateBasin(row, col+1)
-			else:
-				self._populateBasin(row+1, 0)
-			return
-		else:
-			(mnx, mny) = self._mySmallestNeighbor(row,col)
-			if self.basins[mnx][mny] != None:
-				self.basins[row][col] = self.basins[mnx][mny]
-			else:
-				self._populateBasin(mnx, mny)
-				self.basins[row][col] = self.basins[mnx][mny]
-
-			#recurse to next item
-			if col+1 < self.size:
-				self._populateBasin(row, col+1)
-			else:
-				self._populateBasin(row+1, 0)
+    def _mySmallestNeighbor(self, x, y):
+        minVals = []
+        d = {}
+        for k in xrange(4):
+            nx = x + self.neighborx[k]
+            ny = y + self.neighbory[k]
+            if self.isSafe(nx,ny):
+                minVals.append(self.terrain[nx][ny])
+                d[self.terrain[nx][ny]] = (nx, ny)
+        mn = min(minVals)
+        return d[mn]
 
 
-	def showBasin(self):
-		for i in xrange(self.size):
-			sys.stdout.write("\n")
-			for j in xrange(self.size):
-				sys.stdout.write(str(self.basins[i][j]))
-				sys.stdout.write(" ")
-		sys.stdout.write("\n")
+
+    def _populateBasin(self, row, col):
+        if row >= self.size or col >= self.size:
+            #out of bounds
+            return
+        elif self.basins[row][col] != None:
+            #i am already assigned a basin just move ahead
+            if col+1 < self.size:
+                self._populateBasin(row, col+1)
+            else:
+                self._populateBasin(row+1, 0)
+            return
+        else:
+            (mnx, mny) = self._mySmallestNeighbor(row,col)
+            if self.basins[mnx][mny] != None:
+                self.basins[row][col] = self.basins[mnx][mny]
+            else:
+                self._populateBasin(mnx, mny)
+                self.basins[row][col] = self.basins[mnx][mny]
+            #recurse to next item
+            if col+1 < self.size:
+                self._populateBasin(row, col+1)
+            else:
+                self._populateBasin(row+1, 0)
+
+
+    def showBasin(self):
+        for i in xrange(self.size):
+            sys.stdout.write("\n")
+            for j in xrange(self.size):
+                sys.stdout.write(str(self.basins[i][j]))
+                sys.stdout.write(" ")
+        sys.stdout.write("\n")
 
 
 #t = [[1,5,2], [2,4,7], [3,6,9]]
