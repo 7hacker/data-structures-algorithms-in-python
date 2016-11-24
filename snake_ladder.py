097d98bd4105d86c3bce19bd59a68df5026a5362
Nirmal Thacker <nirmalthacker@gmail.com>
2016-11-24 12:57:49 +0530

    snake and ladder

--- snake_ladder.py
+++ snake_ladder.py
@@ -5,6 +5,8 @@ import sys
 
 def shortest_throws(board, size):
     q = [(0,0, "0")]
+    visited = [False] * size
+    visited[0] = True
     while len(q):
         position, pathcost, pathstr = q.pop(0)
         if position == size - 1:
@@ -12,7 +14,8 @@ def shortest_throws(board, size):
         else:
             for i in xrange(1,7):
                 newposition = position + i
-                if newposition < size:
+                if newposition < size and not visited[newposition]:
+                    visited[newposition] = True
                     if board[newposition] != -1:
                         newposition = board[newposition]
                     q.append((newposition, pathcost+1, pathstr+"->"+str(newposition)))
