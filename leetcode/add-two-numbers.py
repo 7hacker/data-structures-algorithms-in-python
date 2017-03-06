937c134b18e1bf0bdca5e822110b1daa51d483c2
Nirmal Thacker <nirmal@jelly.co>
2017-03-06 14:40:29 -0800

    leetcode problem 2

--- leetcode/add-two-numbers.py
+++ leetcode/add-two-numbers.py
@@ -1,8 +1,9 @@
 '''
 https://leetcode.com/problems/add-two-numbers
 '''
+
 # Definition for singly-linked list.
-# class ListNode(object):
+#class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
@@ -14,4 +15,45 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+        t1 = l1
+        t2 = l2
+        carry = False
+        newHead = None
+        newTail = None
+        while t1 or t2:
+            a = 0 if not t1 else t1.val
+            b = 0 if not t2 else t2.val
+            c = a + b #add the two vals at current loc
+            c = c + 1 if carry else c + 0
+
+            v = c % 10 #only get last digit
+
+            newNode = ListNode(v) #make the new node
+            
+            if c > 9: #for next op
+                carry = True
+            else:
+                carry = False
+            
+            if not newHead:
+                newHead = newNode
+                newTail = newNode
+            else:
+                newTail.next = newNode
+                newTail = newNode
+            t1 = t1.next if t1 else None
+            t2 = t2.next if t2 else None
         
+        if carry:
+            newNode = ListNode(1)
+            newTail.next = newNode
+            newTail = newNode
+
+        return newHead
+'''
+if __name__ == '__main__':
+    a, a.next, a.next.next = ListNode(2), ListNode(4), ListNode(3)
+    b, b.next, b.next.next = ListNode(5), ListNode(6), ListNode(4)
+    result = Solution().addTwoNumbers(a, b)
+    print "{0} -> {1} -> {2}".format(result.val, result.next.val, result.next.next.val)
+'''
